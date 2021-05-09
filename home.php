844fa0c2608071f8b527b57b336092a52b22139d
Pxxo.K <69616164+Pxxo@users.noreply.github.com>
2021-05-09 20:02:33 +0900

    Add files via upload

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <div>
+            <h1>迫真ゲーム部裏サイト</h1>
+        </div>
+    </section>
+
+    <section>
+        <h1>NEWS</h1>
+        <p>なんかあった？</p>
+        <p>なんかあった？</p>
+        <p>なんかあった？</p>
+        <p>なんかあった？</p>
+        <p>なんかあった？</p>
+        <p>なんかあった？</p>
+    </section>
+</body>
+
+</html>
